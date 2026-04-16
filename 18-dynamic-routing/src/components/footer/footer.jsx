0f07b3f3b9550ced30f377f816b7b4c73a22b2a9
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-04-16 18:42:57 +0530

    useNavigate

--- 18-dynamic-routing/src/components/footer/footer.jsx
+++ 18-dynamic-routing/src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const footer = () => {
   return (
     <div className='bg-orange-400 w-[90%] rounded-md'>
-        <marquee className="mx-3" behavior="scroll" direction="left">Footer : Nested Routing in React using React Router Dom</marquee>
+        <marquee className="mx-3" behavior="scroll" direction="left">Footer : Dynamic Routing in React using React Router Dom</marquee>
     </div>
   )
 }
--- 18-dynamic-routing/src/pages/contact.jsx
+++ 18-dynamic-routing/src/pages/contact.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const contact = () => {
+  const navigate = useNavigate();
   return (
     <div className='flex justify-center flex-col items-center'>
         <h1 className='text-6xl font-extrabold text-white'>Contact Us</h1>
         <p className='text-white/70'>Email:xymoexyom@gmail.com</p>
+        <div className="flex gap-2 absolute bottom-20">
+      <button className=' bg-white/50 p-3 rounded-xl font-bold shadow-2xl active:scale-95' onClick={()=> {
+        navigate('/')
+      }}>Return to Home page</button>
+      <button className='bg-white/50 p-3 rounded-xl font-bold shadow-2xl active:scale-95' onClick={()=> {
+        navigate(-1);
+      }}>Back</button>
+
+        </div>
     </div>
   )
 }
